526610b95765a9d7c966915f1173d78b99517905
Eugene Burmakin <iamfrey@gmail.com>
2025-05-29 22:12:58 +0200

    Disable http basic auth for mission control jobs

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -79,4 +79,5 @@ Rails.application.configure do
 
   config.active_job.queue_adapter = :solid_queue
   config.solid_queue.connects_to = { database: { writing: :queue } }
+  config.mission_control.jobs.http_basic_auth_enabled = false
 end
